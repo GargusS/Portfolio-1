42bc88bdcf2eaaebe1db909f064571081220c30b
Geir Sonnesyn <geirsonnesyn@gmail.com>
2023-12-08 15:45:04 +0100

    link to gh and live site

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// card01 Gamehub
+
+// Add an event listener to button the github repo
+document.getElementById("myButton01gh").addEventListener("click", function () {
+  // Redirect to the desired URL
+  window.location.href = "https://github.com/GargusS/Gamehub";
+});
+// Add an event listener on button to the live site
+document.getElementById("myButton01ls").addEventListener("click", function () {
+  // Redirect to the desired URL
+  window.location.href = "https://gamehub-unicornpoop.netlify.app/";
+});
+
+// card02 Community Science Museum
+
+// Add an event listener to button the github repo
+document.getElementById("myButton02gh").addEventListener("click", function () {
+  // Redirect to the desired URL
+  window.location.href = "https://github.com/GargusS/SemesterProjectOne";
+});
+// Add an event listener on button to the live site
+document.getElementById("myButton02ls").addEventListener("click", function () {
+  // Redirect to the desired URL
+  window.location.href = "https://semesterprojectone.netlify.app/";
+});
+
+// card03 Famous Robots Corp
+
+// Add an event listener to button the github repo
+document.getElementById("myButton03gh").addEventListener("click", function () {
+  // Redirect to the desired URL
+  window.location.href = "https://github.com/GargusS/project_exam_2023";
+});
+// Add an event listener on button to the live site
+document.getElementById("myButton03ls").addEventListener("click", function () {
+  // Redirect to the desired URL
+  window.location.href = "https://project-exam-23.netlify.app/";
+});
